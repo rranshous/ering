f64be13e4d92d90d00c6227c70a5e17e22ef85eb
Robby Ranshous <rranshous@gmail.com>
2015-09-25 20:29:45 -0400

    revamped ring set implimentation

--- lib/ring_set.rb
+++ lib/ring_set.rb
@@ -4,6 +4,61 @@
 
 module EventRing
   module RingSet
+
+    class Peer
+      def initialize ring1, ring2
+        @in_flight = Hash.new({})
+        Relay.prepend(Filter).new(ring1, ring2, &filter_for(ring1, ring2))
+        Relay.prepend(Filter).new(ring1, ring2, &filter_for(ring2, ring1))
+      end
+
+      private
+
+      # not very sophisticated
+      def filter_for source_ring, target_ring
+        in_flight = @in_flight
+        lambda do |event, data|
+          if in_flight[target_ring].delete([event, data])
+            false
+          else
+            in_flight[source_ring][[event, data]] = :IN_FLIGHT
+            true
+          end
+        end
+      end
+    end
+
+    class Sub
+      def initialize parent_ring, sub_ring, &allow_to_sub_ring
+        Relay.prepend(Filter).new(parent_ring, sub_ring) do |event, data|
+          allow_to_sub_ring.call event, data
+        end
+      end
+    end
+
+    class Relay
+      def initialize source_ring, target_ring
+        @source_ring, @target_ring = source_ring, target_ring
+        @observer = CallIdentifier.new(self, :call_received, @source_ring)
+        @source_ring.join @observer
+      end
+
+      def call_received publishing_ring, event, data
+        @target_ring.publish event, data
+      end
+    end
+
+    module Filter
+      def initialize source_ring, target_ring, &passes_filter
+        @passes_filter = passes_filter
+        super source_ring, target_ring
+      end
+
+      def call_received publishing_ring, event, data
+        super if @passes_filter.call event, data
+      end
+    end
+
     class CallIdentifier
       def initialize target, method, id
         @target = target
@@ -13,32 +68,6 @@ module EventRing
 
       def method_missing *args
         @target.send @method, @id, *args
-      end
-    end
-
-    class Peer
-      def initialize *rings
-        @rings = rings
-        @observers = []
-        initialize_observers
-        @in_flight = Hash.new({})
-      end
-
-      def receiver publishing_ring, *args
-        @rings.each do |ring|
-          next if publishing_ring == ring
-          next if @in_flight[ring].delete(args)
-          @in_flight[ring][args] = 1
-          ring.publish(*args)
-        end
-      end
-
-      private
-
-      def initialize_observers
-        @observers = @rings.map do |ring|
-          CallIdentifier.new(self, :receiver, ring).tap{ |obs| ring.join obs }
-        end
       end
     end
   end
--- spec/ring_set_spec.rb
+++ spec/ring_set_spec.rb
@@ -10,6 +10,36 @@ describe EventRing::RingSet do
   let(:node1) { EventRing::Node.new }
   let(:node2) { EventRing::Node.new }
 
+  describe EventRing::RingSet::Relay do
+    context "two rings attached" do
+      it "passes events from source ring to target ring" do
+        node1.join_ring ring1
+        node2.join_ring ring2
+        expect(node1).to receive(:cascading_event).once
+        expect(node2).to receive(:cascading_event).once
+        expect(node2).to receive(:isolated_event).once
+        described_class.new(ring1, ring2)
+        ring1.publish :cascading_event
+        ring2.publish :isolated_event
+      end
+
+      context "with filter" do
+        it "uses provided block to filter messages" do
+          node1.join_ring ring1
+          node2.join_ring ring2
+          expect(node1).to receive(:for_all).once
+          expect(node2).to receive(:for_all).once
+          expect(node1).to receive(:for_us).once
+          expect(node2).to receive(:for_us).never
+          described_class.prepend(EventRing::RingSet::Filter)
+            .new(ring1, ring2){ |e, d| e == :for_all }
+          ring1.publish :for_all
+          ring1.publish :for_us
+        end
+      end
+    end
+  end
+
   describe EventRing::RingSet::Peer do
     context "two rings attached as peers" do
       it "one ring's published events are received by the other ring's nodes" do
@@ -19,6 +49,24 @@ describe EventRing::RingSet do
         expect(node2).to receive(:testevent).with({ success: 1 }).once
         described_class.new(ring1, ring2)
         ring1.publish :testevent, { success: 1 }
+      end
+    end
+  end
+
+  describe EventRing::RingSet::Sub do
+    context "one ring is a sub ring" do
+      it "filters messages passed to sub ring" do
+        node1.join_ring ring1
+        node2.join_ring ring2
+        expect(node1).to receive(:for_all).with({ success: 2 }).once
+        expect(node2).to receive(:for_all).with({ success: 2 }).once
+        expect(node1).to receive(:for_us).with({ success: 2 }).once
+        expect(node1).to receive(:for_us).with({ success: 2 }).never
+        described_class.new(ring1, ring2) do |event, data|
+          event == :for_all
+        end
+        ring1.publish :for_all, { success: 2 }
+        ring1.publish :for_us, { success: 2 }
       end
     end
   end
